657b3afb254a1e32c0d47aef9a82a85c66cc37e6
Erick Munoz <88725477+Dungyy@users.noreply.github.com>
2021-10-29 00:30:58 -0500

    Add files via upload
    DRUMkit is still at work.
    Uploading what i started with soon will finish and deploy it :D

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
+    switch 
+
+  });
+
+}
+
+const 
+
+    // const audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
